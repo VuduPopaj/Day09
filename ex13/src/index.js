8ca046f888983a75ad27c53a6e734f33fafe666b
VuduPopaj <adnanduljkovic@hotmail.com>
2021-03-16 23:43:58 +0100

    Added ex13

---
+++ ex13/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+function inverseWhile() {
+  var i = 5;
+  var fiveNumbers = "";
+  while (i >= 0) {
+    if (i === 0) {
+      fiveNumbers += i;
+      return console.log(fiveNumbers);
+    }
+    fiveNumbers += i + ", ";
+    i--;
+  }
+}
+inverseWhile();
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
